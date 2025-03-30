16d90081113168a66cf9f30567161d2d802e2e58
naol2025 <abinaol@outlook.com>
2025-03-30 10:56:32 -0700

    removed unsupported arguments

--- storage/main.tf
+++ storage/main.tf
@@ -18,10 +18,10 @@ resource "azurerm_resource_group" "rg" {
 }
 
 module "securestorage" {
-  source               = "app.terraform.io/tffand/securestorage/azurerm"
-  version              = "1.0.0"
-  resource_group_name  = azurerm_resource_group.rg.name
-  location             = azurerm_resource_group.rg.location
+  source  = "app.terraform.io/tffand/securestorage/azurerm"
+  version = "1.0.0"
+  //resource_group_name  = azurerm_resource_group.rg.name
+  //location             = azurerm_resource_group.rg.location
   environment          = "Production"
   storage_account_name = "fandsecurestorage377"
 }
